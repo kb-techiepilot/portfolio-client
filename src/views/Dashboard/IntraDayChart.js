dc10159d781b74592b47bb5495dd3e5c399df34d
balaji <balaji@localhost>
2021-06-11 01:27:23 +0530

    Update IntraDayChart.js

--- src/views/Dashboard/IntraDayChart.js
+++ src/views/Dashboard/IntraDayChart.js
@@ -1,57 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import Chart from 'react-apexcharts';
 import ApexCharts from 'apexcharts';
 
-import Loading from '../../components/Loading';
-import config from '../../config';
-
 function IntraDayChart(props) {
   console.log(props.history);
-    const [data, setData] = useState([]);
-    // const [history, setHistory] = useState([]);
-
-
-    // useEffect(() => {
-    //     axios
-    //     .get(config.apiBaseUrl+"/api/v1/symbols/intraday", {
-    //         params: {
-    //             'symbol': props.symbol,
-    //         }})
-    //     .then(res => {
-    //         setHistory(res.data);
-    //         if(res.data !== []) {
-    //             setLoading(false);
-    //         }
-    //         })
-    //     .catch(err =>{
-    //       console.log(err.message);
-    //     });
-    // },[props.symbol, props.timeLine]);
-
-    // useEffect(() => {
-    //   var history = props.history;
-    //   var responseData = []
-    //   var dataObj = {};
-    //   var data = [];
-    //   // dataObj.data = history;
-      
-    //   var j = 1;
-    //   data[0] = history.intra[0];
-    //   for(var i = 1; i < history.intra.length-1; i++){
-    //       if(history.intra[i][0] - data[j-1][0] > 300000 ){
-    //           data[j] = history.intra[i];
-    //           j++;
-    //       }
-    //   }
-    //   dataObj.data = data;
-    //   dataObj.name = props.symbol;
-    //   responseData.push(dataObj);
-
-    //   setData(responseData);
-    //   console.log(data);
-
-    // },[props.symbol, props.timeLine]);
     
 
     function getLineData(history) {
@@ -60,7 +12,6 @@ function IntraDayChart(props) {
       if(history !== undefined) {
         var dataObj = {};
         var data = [];
-        // dataObj.data = history;
         
         var j = 1;
         data[0] = history.intra[0];
