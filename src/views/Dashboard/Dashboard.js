ed7cb10f435885b8075d51221d45e9db94544f77
balaji <balaji@localhost>
2021-06-11 13:00:38 +0530

    Update Dashboard.js

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+
 import SummaryCards from './SummaryCards';
 import ShareChart from './ShareChart';
 import ContributionChart from './ContributionChart';
 
 function Dashboard(){
+    const { isAuthenticated } = useAuth0();
 
     return(
         <div>
-            <SummaryCards/>
+            {isAuthenticated ? 
+                <SummaryCards/>
+            :
+                <></>
+            }
             <div className="row">
                 <div className="col s12 m6 l6">
                     <ShareChart/>
