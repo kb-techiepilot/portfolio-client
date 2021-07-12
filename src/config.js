0d887d4db711a08180e378ecffb5dc230374824d
balaji <balaji@localhost>
2021-07-12 15:44:13 +0530

    update

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 const config = {
-    apiBaseUrl : 'http://localhost:2000'
+    // apiBaseUrl : 'http://localhost:2000'
+    apiBaseUrl : 'https://kb-shares.azurewebsites.net'
 }
 
 export default config;
